75b61d794e435189ab67da2c46ed4b7b6f5ec2bb
Wen Hong <lamwenhong@yahoo.com.sg>
2020-08-18 01:54:19 +0800

    clear init

--- django_project/utils/__init__.py
+++ django_project/utils/__init__.py
@@ -1,11 +1,1 @@
-from bs4 import BeautifulSoup
 import logging
-
-#logger = logging.getLogger("samplelogger")
-soup = None
-
-def init():
-	with open("/var/www/react/build/index.html") as fp:
-		soup = BeautifulSoup(fp)
-
-#init()
