ef3ac42b47cfdff69a9dbaef3c7ed91140894c0d
wenhong <lamwenhong@yahoo.com.sg>
2020-09-18 23:38:19 +0800

    add examples

--- django_project/utils/urls.py
+++ django_project/utils/urls.py
@@ -5,6 +5,11 @@ from . import views
 	TODO: 
 	1. Add URL subpaths here.
 	2. Write functions in views.py to handle the various subpaths that you added.
+
+	Examples of other subpaths cound be:
+    - path('post/<post_id>', views.posts, name='posts')
+    - path('data/<date>/<time>', views.data, name='data')
+	And then we would need to define the methods post(post_id) and data(date,time) in views.py to return the correct HttpResponse.
 """
 
 urlpatterns = [
