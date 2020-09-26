b424dc4166d0bb5620d6bb22a2a7a00f659a9894
wenhong <lamwenhong@yahoo.com.sg>
2020-09-26 12:49:53 +0800

    update init

--- django_project/utils/__init__.py
+++ django_project/utils/__init__.py
@@ -4,12 +4,12 @@ from django.core.cache import cache
 
 
 #Get soup object from index.html
-def setSoupObject():
+def set_soup_object():
 	with open("/var/www/react/build/index.html") as fp:
 		soup = BeautifulSoup(fp,features="html.parser")
 	cache.set("index_base",soup,timeout=None)
 
 
 logger = logging.getLogger(__name__)
-setSoupObject()
+set_soup_object()
 logger.info("Cached index.html as BS object.")
